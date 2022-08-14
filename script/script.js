cbe60526eaf943c415a212029bb80b02686e6204
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2022-08-13 22:33:54 -0300

    requisitos: botao finalizar e envio do pedido

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// Main Functions
+
 function check(menuId, opt) {
     const menu = document.querySelector(`#${menuId}`)
     icon = opt.children[2];
@@ -19,6 +21,62 @@ function check(menuId, opt) {
     verifyEnbaleButton();
 }
 
+
+function closeOrder() {
+    const menuArray = document.querySelectorAll(`.checked`);
+    const modalText = document.querySelector(`#modal-text`); 
+    modalText.children[3].children[1].innerHTML = 0;
+    
+    // Armazena o nome e o preço das opções selecionadas
+    let modalItem;
+    let modalItemPrice;
+    let menuItem;
+    let menuItemPrice;
+    let formattedString;
+    
+    for (let i = 0; i < 3; i++) {
+        
+        menuItem = menuArray[i].children[1].children[0];
+        menuItemPrice = menuArray[i].children[1].children[2];  
+        formattedString = menuItemPrice.textContent.replace("R$", "");
+        
+        
+        modalItem = modalText.children[i].children[0];
+        modalItemPrice = modalText.children[i].children[1]
+        
+        modalItemPrice.textContent = formattedString;
+        modalItem.textContent = menuItem.textContent;
+        
+        incrementsTotal(formattedString) 
+    }
+    
+    const modalWindow = document.querySelector(`.modal`);
+    modalWindow.classList.remove("hidden");
+}
+
+function confirmOrder() {
+    const arrayOpt = document.querySelectorAll('.checked');
+    const prato = arrayOpt[0].children[1].children[0].innerHTML;
+    const bebida = arrayOpt[1].children[1].children[0].innerHTML;
+    const sobremesa = arrayOpt[2].children[1].children[0].innerHTML;
+
+    const total = (document.querySelector('#modal-text').children[3].children[1].innerHTML).replace("$", "$ ").replace(",", ".");
+
+    const str = `Olá, gostaria de fazer o pedido:\n- Prato: ${prato}\n- Bebida: ${bebida}\n- Sobremesa: ${sobremesa}\nTotal: ${total}`
+    
+    const strEncode = encodeURIComponent(str);
+    console.log(strEncode)
+
+    window.open(`https://wa.me/5514997928787?text=${strEncode}`)
+}
+
+function cancelOrder() {
+    const modal = document.querySelector(`.modal`);
+    modal.classList.add("hidden");
+}
+
+// Auxiliary Functions
+
 function verifyEnbaleButton() {
     const menu1 = document.querySelector("#main-course-menu");
     const menu2 = document.querySelector("#drink-menu");
@@ -35,6 +93,11 @@ function verifyEnbaleButton() {
     }
 }
 
-function closeOrder() {
-    alert('fui clicado');
+function incrementsTotal(priceString){
+    const modalTotal = document.querySelector("#modal-text").children[3].children[1];
+    let modalTotalPrice = Number(modalTotal.textContent.replace("R$", "").replace(",", '.'));
+
+    modalTotalPrice += Number(priceString.replace(',', '.'));
+    
+    modalTotal.textContent = `R$${modalTotalPrice.toFixed(2).replace(".", ",")}`;
 }
